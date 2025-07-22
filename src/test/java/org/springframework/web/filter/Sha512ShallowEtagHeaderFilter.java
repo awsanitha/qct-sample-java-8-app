d1e1bbd0d45a72d82cd38d4570aa4074bc0b483b
Version Upgrade <n/a>
2025-07-22 06:54:29 +0000

    Automated commit for Q Code Transformation Job: 2f234376-e662-4cdd-97e5-c21474f702d6

--- src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
+++ src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
@@ -2,12 +2,23 @@ package org.springframework.web.filter;
 
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+/**
+ * Custom implementation of ShallowEtagHeaderFilter that uses SHA-512 for ETag generation
+ */
 public class Sha512ShallowEtagHeaderFilter extends ShallowEtagHeaderFilter {
 
-	@Override
-	protected String generateETagHeaderValue(byte[] bytes) {
-		final HashCode hash = Hashing.sha512().hashBytes(bytes);
+    /**
+     * Generate an ETag value using SHA-512 algorithm
+     */
+    protected String generateETagValue(byte[] responseBody) {
+        final HashCode hash = Hashing.sha512().hashBytes(responseBody);
 		return "\"" + hash + "\"";
 	}
 }
