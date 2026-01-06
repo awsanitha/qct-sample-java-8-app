d42dc7638940076d2be65e7c39e3ba53ca47e840
Version Upgrade <n/a>
2026-01-06 08:52:51 +0000

    Step 4: Update Spring Boot Parent and Core Spring Dependencies Build status: Success

--- src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
+++ src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
@@ -2,12 +2,19 @@ package org.springframework.web.filter;
 
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class Sha512ShallowEtagHeaderFilter extends ShallowEtagHeaderFilter {
 
 	@Override
-	protected String generateETagHeaderValue(byte[] bytes) {
-		final HashCode hash = Hashing.sha512().hashBytes(bytes);
-		return "\"" + hash + "\"";
+	protected String generateETagHeaderValue(InputStream inputStream, boolean isWeak) {
+		try {
+			byte[] bytes = inputStream.readAllBytes();
+			final HashCode hash = Hashing.sha512().hashBytes(bytes);
+			return "\"" + hash + "\"";
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to generate ETag", e);
+		}
 	}
 }
