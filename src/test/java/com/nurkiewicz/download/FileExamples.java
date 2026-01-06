d33a85fa0ab2cacaccae7eb7f01eee8c58c2be70
Version Upgrade <n/a>
2026-01-06 09:38:38 +0000

    Upgrade to Java 21 on Iteration 2

--- src/test/java/com/nurkiewicz/download/FileExamples.java
+++ src/test/java/com/nurkiewicz/download/FileExamples.java
@@ -6,9 +6,9 @@ import java.util.UUID;
 
 public class FileExamples {
 
-	public static final UUID TXT_FILE_UUID = UUID.randomUUID();
+	public static final UUID TXT_FILE_UUID = UUID.fromString("8f4d5c27-44d4-4bbb-8279-d4facbc59658");
 	public static final FilePointer TXT_FILE = txtFile();
-	public static final UUID NOT_FOUND_UUID = UUID.randomUUID();
+	public static final UUID NOT_FOUND_UUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
 
 	private static FileSystemPointer txtFile() {
 		final URL resource = FileStorageStub.class.getResource("/download.txt");
