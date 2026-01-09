2b0d6ee875ee58fbcc2a1f30f9169ac29f648d34
Version Upgrade <n/a>
2026-01-09 07:16:11 +0000

    Step 7: Update Spring Boot and Spring Framework Ecosystem Build status: Success

--- src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
+++ src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
@@ -2,12 +2,17 @@ package org.springframework.web.filter;
 
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
+import java.io.InputStream;
 
 public class Sha512ShallowEtagHeaderFilter extends ShallowEtagHeaderFilter {
 
 	@Override
-	protected String generateETagHeaderValue(byte[] bytes) {
-		final HashCode hash = Hashing.sha512().hashBytes(bytes);
-		return "\"" + hash + "\"";
+	protected String generateETagHeaderValue(InputStream inputStream, boolean isWeak) {
+		try {
+			final HashCode hash = Hashing.sha512().hashBytes(inputStream.readAllBytes());
+			return "\"" + hash + "\"";
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
