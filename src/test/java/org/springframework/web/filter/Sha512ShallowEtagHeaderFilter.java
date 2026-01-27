ba1d0a59ebc205a162ecf1ca5f60267a7f27d26c
Version Upgrade <n/a>
2026-01-27 09:40:36 +0000

    Step 12: Complete Integration Verification and Documentation Build status: Success

--- src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
+++ src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
@@ -2,11 +2,16 @@ package org.springframework.web.filter;
 
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
+import com.google.common.io.ByteSource;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Sha512ShallowEtagHeaderFilter extends ShallowEtagHeaderFilter {
 
 	@Override
-	protected String generateETagHeaderValue(byte[] bytes) {
+	protected String generateETagHeaderValue(InputStream inputStream, boolean isWeak) throws IOException {
+		byte[] bytes = ByteSource.wrap(inputStream.readAllBytes()).read();
 		final HashCode hash = Hashing.sha512().hashBytes(bytes);
 		return "\"" + hash + "\"";
 	}
