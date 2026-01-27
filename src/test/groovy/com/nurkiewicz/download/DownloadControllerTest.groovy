6e12651265cb471fd8d5c96d69a305ca86fdb49a
Version Upgrade <n/a>
2026-01-27 10:19:47 +0000

    Upgrade to Java 21 on Iteration 3

--- src/test/groovy/com/nurkiewicz/download/DownloadControllerTest.groovy
+++ src/test/groovy/com/nurkiewicz/download/DownloadControllerTest.groovy
@@ -1,10 +1,12 @@
 package com.nurkiewicz.download
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 import spock.lang.Specification
 
 import java.time.Instant
@@ -17,15 +19,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
-@SpringBootTest(classes = [MainApplication])
-@AutoConfigureMockMvc
+@SpringBootTest
+@ContextConfiguration(classes = [MainApplication])
 @ActiveProfiles("test")
 class DownloadControllerSpec extends Specification {
 
 	@Autowired
-	private MockMvc mockMvc
+	WebApplicationContext wac
+
+	MockMvc mockMvc
 
 	private static final String TEXT_FILE = '/download/' + FileExamples.TXT_FILE_UUID + '/file.txt';
+
+	def setup() {
+		if (wac != null) {
+			mockMvc = MockMvcBuilders.webAppContextSetup(wac).build()
+		}
+	}
 
 	def 'should return bytes of existing file'() {
 		expect:
