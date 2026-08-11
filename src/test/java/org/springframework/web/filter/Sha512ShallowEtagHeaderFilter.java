181a852e5bcadce8f1fee1938819d523a35dc47b
Version Upgrade <n/a>
2026-08-11 06:21:44 +0000

    Upgrade to Java 25 on Iteration 2

--- src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
+++ src/test/java/org/springframework/web/filter/Sha512ShallowEtagHeaderFilter.java
@@ -3,11 +3,19 @@ package org.springframework.web.filter;
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Sha512ShallowEtagHeaderFilter extends ShallowEtagHeaderFilter {
 
 	@Override
-	protected String generateETagHeaderValue(byte[] bytes) {
-		final HashCode hash = Hashing.sha512().hashBytes(bytes);
-		return "\"" + hash + "\"";
+	protected String generateETagHeaderValue(InputStream inputStream, boolean isWeak) throws IOException {
+		final HashCode hash = Hashing.sha512().hashBytes(inputStream.readAllBytes());
+		StringBuilder builder = new StringBuilder();
+		if (isWeak) {
+			builder.append("W/");
+		}
+		builder.append("\"").append(hash).append("\"");
+		return builder.toString();
 	}
 }
